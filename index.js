de683b8c98b2118a3a66ba0fecf012809d143200
joakom24 <joakoomussi@gmail.com>
2022-01-12 20:36:13 -0300

    eliminar

--- index.js
+++ index.js
@@ -162,40 +162,7 @@ client.on("message", async (message) => {
       );
     }
   } else if (command === "eliminar") {
-    if (!message.guild.me.permissionsIn(message.channel).has("MANAGE_MESSAGES"))
-      return message.channel.send("Perdon, pero no tengo permisos");
-
-    if (!message.member.permissionsIn(message.channel).has("MANAGE_MESSAGES"))
-      return message.channel.send("Perdon, pero no tienes permisos");
-
-    if (!args[0])
-      return message.channel.send("Escriba la cantidad de mensajes a eliminar");
-    let cantidad = parseInt(args[0]);
-
-    if (!cantidad || isNaN(cantidad))
-      return message.channel.send("Introduce un numero por favor");
-
-    if (cantidad > 100) {
-      message.channel.send(
-        "El maximo de mensajes que puedo borrar es 100, por lo tanto lo establecere automaticamente ahi"
-      );
-      cantidad = 100;
-    }
-
-    message.channel.messages.fetch({ limit: cantidad }).then((mensajes) => {
-      message.channel
-        .bulkDelete(mensajes.filter((m) => !m.pinned))
-        .then(() => {
-          message.channel.send(
-            `Listo, borre los ${cantidad} mensajes :ok_hand:`
-          );
-        })
-        .catch((e) => {
-          message.channel.send(
-            "Ocurrio un error y no pude borrar los mensajes"
-          );
-        });
-    });
+    client.commands.get("eliminar").execute(message, args);
   } else {
     message.channel.send(
       "Escribí un comando que exista la concha de tu madre."
