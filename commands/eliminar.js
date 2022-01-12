b9c1088ac3efd6f87c8a9b49e1d623a337e82a7a
joakom24 <joakoomussi@gmail.com>
2022-01-12 20:52:52 -0300

    eliminarparte2

--- commands/eliminar.js
+++ commands/eliminar.js
@@ -3,23 +3,27 @@ module.exports = {
   alias: ["e"],
   execute(message, args) {
     if (!message.guild.me.permissionsIn(message.channel).has("MANAGE_MESSAGES"))
-      return message.channel.send("Perdon, pero no tengo permisos");
+      return message.channel.send("Pero no tienes permisos hermano");
 
     if (!message.member.permissionsIn(message.channel).has("MANAGE_MESSAGES"))
-      return message.channel.send("Perdon, pero no tienes permisos");
+      return message.channel.send("Pero no tienes permisos hermano");
 
     if (!args[0])
-      return message.channel.send("Escriba la cantidad de mensajes a eliminar");
+      return message.channel.send(
+        "Te pensas que soy adivino? Escribí el comando y después un numero por favor"
+      );
     let cantidad = parseInt(args[0]);
 
     if (!cantidad || isNaN(cantidad))
-      return message.channel.send("Introduce un numero por favor");
-
-    if (cantidad > 100) {
-      message.channel.send(
-        "El maximo de mensajes que puedo borrar es 100, por lo tanto lo establecere automaticamente ahi"
+      return message.channel.send(
+        "Te pensas que soy adivino? Escribí el comando y después un numero por favor"
       );
-      cantidad = 100;
+
+    if (cantidad > 500) {
+      message.channel.send(
+        "El maximo de mensajes que puedo borrar es 500 pelotudo"
+      );
+      cantidad = 500;
     }
 
     message.channel.messages.fetch({ limit: cantidad }).then((mensajes) => {
@@ -27,13 +31,11 @@ module.exports = {
         .bulkDelete(mensajes.filter((m) => !m.pinned))
         .then(() => {
           message.channel.send(
-            `Listo, borre los ${cantidad} mensajes :ok_hand:`
+            `Listo, borre los ${cantidad} mensajes :ok_hand:, de nada`
           );
         })
         .catch((e) => {
-          message.channel.send(
-            "Ocurrio un error y no pude borrar los mensajes"
-          );
+          message.channel.send("No quiero borrar jeee");
         });
     });
   },
